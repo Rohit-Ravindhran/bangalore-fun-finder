994766e78a8f2b2b953d483e0d264e1020c6be83
Rohit Ravindhran <68892714+Rohit-Ravindhran@users.noreply.github.com>
2025-05-10 12:51:48 +0530

    Issue fix

--- src/components/CategoryFilter.tsx
+++ src/components/CategoryFilter.tsx
@@ -27,7 +27,7 @@ const CategoryFilter: React.FC<CategoryFilterProps> = ({
   
   return (
     <div className="relative">
-      <ScrollArea className="w-full py-2 pb-4  w-[90%]  overflow-x-auto">
+      <ScrollArea className="w-full py-2 pb-4  max-w-[300px]  overflow-x-auto">
         <div className="flex space-x-2.5 px-3 ">
           <button
             onClick={onSelectAll}
