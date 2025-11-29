2d8f3615d6d1347ca805cda0c490e5be37c3b9d3
Rohit <rohit@getomnify.com>
2025-11-29 19:09:20 +0530

    Comment remove

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       staleTime: 5 * 60 * 1000, // 5 minutes
-      cacheTime: 10 * 60 * 1000, // 10 minutes
+      cacheTime: 10 * 60 * 1000, // 10 minute
       refetchOnWindowFocus: false,
     },
   },
